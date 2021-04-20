bf253c0ebaf22b2500108c564f13a910857def9a
ogubuike alex <ogubuikealex@gmail.com>
2021-04-21 00:56:51 +0100

    Selected Elements to be worked on

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//grab all the editables
+plasey = document.querySelector(".toggle");
+range = document.querySelectorAll("input");
+skipper = document.querySelectorAll("[data-skip]");
+progressBar = document.querySelector(".progress__filled");
+progress = document.querySelector(".progress");
+
+console.log(plasey, range, skipper, progress)
+//adding all functions
+
+//adding eventlisteners
