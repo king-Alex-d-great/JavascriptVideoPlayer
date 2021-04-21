66bc3aa3caf13d77d408b3ffe176f043376cc03e
ogubuike alex <ogubuikealex@gmail.com>
2021-04-21 11:17:31 +0100

    Full screen functionality added

--- scripts.js
+++ scripts.js
@@ -3,40 +3,50 @@ let plasey = document.querySelector(".toggle");
 let range = document.querySelectorAll("input");
 let skipper = document.querySelectorAll("[data-skip]");
 let progressBar = document.querySelector(".progress__filled");
- let progress = document.querySelector(".progress");
- let video =  document.querySelector(".viewer");
+let progress = document.querySelector(".progress");
+let video = document.querySelector(".viewer");
+let fullScreen = document.querySelector(".full-screen");
+let player = document.querySelector(".player");
 
-console.dir(video)
+console.dir(video);
 //adding all functions
 
 //play and pause function
- function handlePlay(){
-    if (video.paused){
-       plasey.innerHTML = "⏸"  
-       video.play()
-        return
-    }
-    plasey.innerHTML = "▶"
-    video.pause();
-    
-    //let toggle = video.paused ? "play" : "pause"
-    //video[toggle]();
- }
+function handlePlay() {
+  if (video.paused) {
+    plasey.innerHTML = "⏸";
+    video.play();
+    return;
+  }
+  plasey.innerHTML = "▶";
+  video.pause();
 
- //volume and speed function
- function handleSliders () {
-   video[this.name] = this.value;
- }
+  //(video.paused) ? plasey.innerHTML = "⏸" && video.play() :  plasey.innerHTML = "▶" && video.pause();
+//or
+  //let toggle = video.paused ? "play" : "pause"
+  //video[toggle]();
+}
 
- //skipping functions
- function handleSkipper () {
-video.currentTime += +(this.dataset.skip);
- }
+//volume and speed function
+function handleSliders() {
+  video[this.name] = this.value;
+}
 
+//skipping functions
+function handleSkipper() {
+  video.currentTime += +this.dataset.skip;
+}
 
+//make full screen
+function handleScreen() {
+  player.width === window.innerWidth
+    ? player.classList.toggle("screen")
+    : (player.width = window.innerWidth);
+}
 
 //adding eventlisteners
 plasey.addEventListener("click", handlePlay);
 video.addEventListener("click", handlePlay);
-range.forEach(range => range.addEventListener("change", handleSliders))
-skipper.forEach(skip => skip.addEventListener("click", handleSkipper))
+range.forEach((range) => range.addEventListener("change", handleSliders));
+skipper.forEach((skip) => skip.addEventListener("click", handleSkipper));
+fullScreen.addEventListener("click", handleScreen);
